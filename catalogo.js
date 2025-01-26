9ebbf45c1bb17135d1cf661e3d249fcfaebbea22
JMurilo <j.murilo.87@gmail.com>
2025-01-26 20:43:16 -0300

    update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,2 +1,4 @@
+// ainda estou aqui 2024 drama cam 14
 const link = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/N2VjM2E0NjEtMWM4Ny00NjVjLTg2ZWEtNjQ5YmFiN2FiNzZk.m3u8';
+// canina 2024 drama hd 18
 const link2 = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/NDQwMGMwYmMtYzBhZC00NWI2LTk5ZDAtMWI2MWUwZjU5NDgw.m3u8';
