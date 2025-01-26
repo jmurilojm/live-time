15a4bd31ef287e454fbc492ccea06b153e49e252
JMurilo <j.murilo.87@gmail.com>
2025-01-26 20:56:23 -0300

    update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,4 +1,25 @@
 // ainda estou aqui 2024 drama cam 14
 const link = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/N2VjM2E0NjEtMWM4Ny00NjVjLTg2ZWEtNjQ5YmFiN2FiNzZk.m3u8';
+
 // canina 2024 drama hd 18
 const link2 = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/NDQwMGMwYmMtYzBhZC00NWI2LTk5ZDAtMWI2MWUwZjU5NDgw.m3u8';
+
+// a vigia 2023 L hd terror
+const link3 = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/NGMxZWQ5MGQtYjYxNi00OTQ1LTliNDItOTUzYjQyZTEwYWYz.m3u8'
+//imagem: https://cdn.syncdev.com.br//uploads/video_thumb/13778.jpg
+
+// o assassino do calendario 2025 16 drama hd
+const link4 = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/YTk3MmJjMzQtYzE5Mi00MzQwLWJkNWEtOGY0MDgzNTdlZGFj.m3u8'
+//imagem: https://cdn.syncdev.com.br//uploads/video_thumb/13735.jpg
+
+//Mufasa: o rei leao 2024 L cam aventura
+const link5 = 'http://vods.conexao-vs.live:80/movie/655610.mp4'
+//imagem: https://cdn.syncdev.com.br//uploads/video_thumb/13547.jpg
+
+// Lobisomem 2025 16 cam terror
+const link6 = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/NDY2MGEwNTQtZTE5Zi00MTEwLWExYTEtNjIzZDYxNmNmYjgx.m3u8'
+//imagem: https://cdn.syncdev.com.br//uploads/video_thumb/13769.jpg
+
+// o cacador 2024 16 acao hd
+const link6 = 'https://sinalprivado.info/m3u8/MQ==/dnotNTgwOTllNWQtZGM2/NzUyOTU5YTctNmRiNy00MDAxLTlkMmItMjM2YjZjYjY4YmZj.m3u8'
+//imagem: https://cdn.syncdev.com.br//uploads/video_thumb/13521.jpg
